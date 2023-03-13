63130dbf634acafdd59c1c033bc44f145d9d6b78
Nicolas Degen <ndegen@me.com>
2023-03-13 18:50:28 +0100

    Add blur to nav bar

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -16,7 +16,7 @@ export default () => {
 
   return (
     <header>
-      <nav className="h-20 py-3 px-4 flex items-center justify-between w-full fixed bg-white">
+      <nav className='h-20 py-3 px-4 flex items-center justify-between w-full fixed backdrop-blur-md bg-white/70' >
         <Link className="nav-brand" href="/">
           <img
             src="/images/logo125.png"
@@ -30,11 +30,7 @@ export default () => {
           {isOpen ? <IconX /> : <IconMenu />}
         </button>
 
-        <div
-          className={`absolute top-20 left-0 px-6 pb-3 ${
-            isOpen ? '' : 'hidden'
-          } bg-white w-full  md:p-0 md:w-auto md:mr-2 md:top-0 md:relative md:block`}
-        >
+        <div className={`absolute top-20 left-0 px-6 pb-3 ${isOpen ? 'bg-white/70 backdrop-blur-md' : 'hidden' } w-full md:p-0 md:w-auto md:mr-2 md:top-0 md:relative md:block`} >
           <ul className="flex justify-between flex-col w-full gap-3 md:flex-row md:gap-6">
             <li>
               <Link className={styles.navLink} href="/#features">
